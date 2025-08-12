31302938b84dcf90570b914cac4f510c7daa9053
Leandro Ucuamba <leandrodossantos.lc5@gmail.com>
2025-08-12 19:11:42 +0100

    Add project architecture

---
+++ src/main/java/com/api/parking_control/controllers/ParkingSpotController.java
@@ -0,0 +1,21 @@
+package com.api.parking_control.controllers;
+
+import com.api.parking_control.repositories.ParkingSpotRepository;
+import com.api.parking_control.services.ParkingSpotService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RequestMapping("/parking-spot")
+public class ParkingSpotController {
+
+    @Autowired
+    private ParkingSpotService parkingSpotService;
+
+    public ParkingSpotController(ParkingSpotService parkingSpotService) {
+        this.parkingSpotService = parkingSpotService;
+    }
+}
---
+++ src/main/java/com/api/parking_control/services/ParkingSpotService.java
@@ -0,0 +1,17 @@
+package com.api.parking_control.services;
+
+import com.api.parking_control.repositories.ParkingSpotRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ParkingSpotService {
+
+    private ParkingSpotRepository parkingSpotRepository;
+
+    public ParkingSpotService(ParkingSpotRepository parkingSpotRepository) {
+        this.parkingSpotRepository = parkingSpotRepository;
+    }
+
+
+}
